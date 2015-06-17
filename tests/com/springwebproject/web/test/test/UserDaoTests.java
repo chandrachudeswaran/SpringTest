fa5fb54edb9eca468b9772f62d4055972e80d51b
Chandrachudeswaran <chandrachudeswaran@gmail.com>
2015-06-17 18:44:13 -0400

    Added the Junits test cases for UsersDao

--- tests/com/springwebproject/web/test/test/UserDaoTests.java
+++ tests/com/springwebproject/web/test/test/UserDaoTests.java
@@ -1,12 +1,23 @@
 package com.springwebproject.web.test.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.springwebproject.dao.User;
+import com.springwebproject.dao.UserDao;
 
 @ActiveProfiles("dev")
 @ContextConfiguration(locations = {
@@ -16,10 +27,33 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserDaoTests {
 	
+	@Autowired
+	private UserDao usersdao;
+	@Autowired
+	private DataSource datasource;
+	
+	@Before
+	public void init(){
+		JdbcTemplate jdbc= new JdbcTemplate(datasource);
+		
+		jdbc.execute("delete from users");
+		jdbc.execute("delete from authorities");
+		
+	}
 	@Test
 	public void testCreateUser(){
+		User user = new User("chandar","chandar@gmail.com","viji",true,"user");
+		assertTrue("created",usersdao.createUser(user));
 		
-		assertEquals("Dummy",1,1);
+		
+		List<User> users = usersdao.getAllUsers();
+	assertEquals("Number of users should be 1",1,users.size());
+	
+	
+	assertTrue("users should exist",usersdao.exists(user.getUsername()));
+	
+	
+	assertEquals("Retreived user should be identical to created user" ,user,users.get(0));
 	}
 
 }
